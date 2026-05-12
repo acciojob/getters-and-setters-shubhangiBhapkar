efb6986a2d842c4c37ea9b9d16b5c2bd7c1fa8ca
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-05-12 11:05:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this.name=name;
-		this.age=age;
+		this._name=name;
+		this._age=age;
 	}
 	get getName(){
-		return this.name;
+		return this._name;
 	}
 
 	set setAge(age){
-		this.age = age;
+		this._age = age;
 	}
 }
 
