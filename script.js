b6963cfad66677226cae7e18c5ce7d93c26d3378
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-07-06 19:51:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,55 +1,36 @@
 class Person {
-	
-	constructor(name, age) {
-		this._name = name;
-		this._age = age;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-	
-	get Name() {
-		return this._name;
-	}
-	set name(value) {
+  get name() {
+    return this._name;
+  }
+  set name(value) {
     this._name = value;
   }
 
-	get age() {
+  get age() {
     return this._age;
   }
-	
-	set Age(age) {
-		this._age = age;
-	}
+  set age(value) {
+    this._age = value;
+  }
 }
 
 class Student extends Person {
-	
-	study() {
-		console.log(`${this._name} is studying`);
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	
-	teach() {
-		console.log(`${this._name} is teaching`);
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
-/ Creating a Person instance
-const person = new Person("John", 25);
-console.log(person.name);  // Output: John
 
-person.age = 30;  // Using the setter to change the age
-console.log(person.age);  // Output: 30
-
-// Creating a Student instance
-const student = new Student("Alice", 22);
-student.study();  // Output: Alice is studying
-
-// Creating a Teacher instance
-const teacher = new Teacher("Bob", 40);
-teacher.teach();  // Output: Bob is teaching
-// Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
