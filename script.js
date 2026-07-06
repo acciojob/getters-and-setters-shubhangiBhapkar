027a76652ee867ca2f1ef5f399f2e6a96248aca1
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-07-06 19:41:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,6 +34,20 @@ class Teacher extends Person {
 		console.log(`${this._name} is teaching`);
 	}
 }
+// Creating a Person instance
+const person = new Person("John", 25);
+console.log(person.name);  // Output: John
+
+person.age = 30;  // Using the setter to change the age
+console.log(person.age);  // Output: 30
+
+// Creating a Student instance
+const student = new Student("Alice", 22);
+student.study();  // Output: Alice is studying
+
+// Creating a Teacher instance
+const teacher = new Teacher("Bob", 40);
+teacher.teach();  // Output: Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
